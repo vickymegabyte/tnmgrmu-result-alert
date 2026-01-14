6bd6ce778f0827673e300dbd71e173c3585f2c9f
vickymegabyte <studentpharm00@gmail.com>
2026-01-14 17:19:57 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,28 +1,33 @@
 import requests
 import hashlib
+import os
 
 URL = "https://cms2results.tnmgrmuexam.ac.in/#/ExamResult"
+
+TOKEN = os.environ["tnmgrmu_result_alert_bot"]
+CHAT_ID = os.environ["6780399594"]
 
 def get_hash():
     r = requests.get(URL, timeout=20)
     return hashlib.md5(r.text.encode()).hexdigest()
 
-import os
-TOKEN = os.environ["tnmgrmu_result_alert_bot"]
-CHAT_ID = os.environ["6780399594"]
-:
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    requests.post(url, json={"chat_id": chat_id, "text": msg})
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": msg
+    })
 
 def main():
     with open("hash.txt", "r") as f:
-        old_hash = f.read()
+        old_hash = f.read().strip()
 
     new_hash = get_hash()
+
     if new_hash != old_hash:
         send_telegram(
-            "🚨 TNMGRMU Result Update Detected!\n\nhttps://cms2results.tnmgrmuexam.ac.in/#/ExamResult",
-            TOKEN, CHAT_ID
+            "🚨 TNMGRMU Result Update Detected!\n\n"
+            "https://cms2results.tnmgrmuexam.ac.in/#/ExamResult"
         )
         with open("hash.txt", "w") as f:
             f.write(new_hash)
